7e469c4695ab5a0e7720e14ba2f5f2c769e6c6c1
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-09-07 12:34:26 +0000

    initial hw1

--- fl23/hw1.py
+++ fl23/hw1.py
@@ -1,9 +1,20 @@
+import duckdb
 import pandas as pd
 
 
-def get_adults(people: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame()
+def filter_after_cutoff_age(people: pd.DataFrame, cutoff_age: int) -> pd.DataFrame:
+    """Filter a data frame of people to only include people older than a cutoff age."""
+    return people[people["Age"] >= cutoff_age]
 
 
-def avg_per_state(people: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame()
+def avg_per_state(people: pd.DataFrame) -> pd.DataFrame:  # noqa: ARG001
+    """Compute the average age per state."""
+    return duckdb.sql(
+        """
+        SELECT
+            State,
+            AVG(Age),
+        FROM people
+        GROUP BY State
+        """,
+    ).df()
--- tests/test_hw_1.py
+++ tests/test_hw_1.py
@@ -1,12 +1,18 @@
 import pandas as pd
+
 from fl23 import hw1
 
 
-def test_get_adults():
+def test_filter_after_cutoff_age():
+    cutoff_age = 18
     people = pd.DataFrame({"Age": [17, 18]})
-    assert hw1.get_adults(people) == pd.DataFrame({"Age": [18]})
+    adults = hw1.filter_after_cutoff_age(people, cutoff_age)
+    assert all(adults["Age"] >= cutoff_age)
 
 
 def test_avg_per_state():
     people = pd.DataFrame({"State": ["MO", "MO", "IL"], "Age": [17, 18, 19]})
-    assert hw1.avg_per_state(people) == {"MO": 17.5, "IL": 19}
+    pd.testing.assert_frame_equal(
+        hw1.avg_per_state(people),
+        pd.DataFrame({"State": ["IL", "MO"], "avg(Age)": [19, 17.5]}),
+    )
