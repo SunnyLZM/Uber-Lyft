2f12b93b786b52a2a4cf61b5a95f069b32da8c54
Ty Schlichenmeyer <t.schlic@wustl.edu>
2023-09-09 02:49:32 +0000

    add test for p2

--- tests/test_autograde.py
+++ tests/test_autograde.py
@@ -12,3 +12,13 @@ def test_1_uses_sql(mocker):
 
     assert sql_spy.call_count == 1
     assert pandas_spy.call_count == 0
+
+
+def test_2_uses_pandas(mocker):
+    sql_spy = mocker.spy(duckdb, "sql")
+    pandas_spy = mocker.spy(pd.DataFrame, "query")
+
+    hw1.filter_after_cutoff_age(pd.DataFrame({"Age": [17, 18]}), 18)
+
+    assert sql_spy.call_count == 0
+    assert pandas_spy.call_count == 1
