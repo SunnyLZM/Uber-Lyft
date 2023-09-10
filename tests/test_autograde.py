1e9b329d4e47ab40110771ef8710f606c35421d5
Ty Schlichenmeyer <t.schlic@wustl.edu>
2023-09-10 05:47:20 +0000

    fix: wrong function in test_2_uses_pandas

--- tests/test_autograde.py
+++ tests/test_autograde.py
@@ -18,7 +18,9 @@ def test_2_uses_pandas(mocker):
     sql_spy = mocker.spy(duckdb, "sql")
     pandas_spy = mocker.spy(pd.DataFrame, "query")
 
-    hw1.filter_after_cutoff_age(pd.DataFrame({"Age": [17, 18]}), 18)
+    people = pd.DataFrame({"State": ["MO", "MO", "IL"], "Age": [17, 18, 19]})
+
+    hw1.avg_per_state(people)
 
     assert sql_spy.call_count == 0
     assert pandas_spy.call_count == 1
