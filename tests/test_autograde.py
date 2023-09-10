3e89fa2816b40f3baac95ea6764af3681256bbc8
Ty Schlichenmeyer <t.schlic@wustl.edu>
2023-09-10 06:08:51 +0000

    remove pandas spy

--- tests/test_autograde.py
+++ tests/test_autograde.py
@@ -6,21 +6,17 @@ from fl23 import hw1
 
 def test_1_uses_sql(mocker):
     sql_spy = mocker.spy(duckdb, "sql")
-    pandas_spy = mocker.spy(pd.DataFrame, "query")
 
     hw1.filter_after_cutoff_age(pd.DataFrame({"Age": [17, 18]}), 18)
 
     assert sql_spy.call_count == 1
-    assert pandas_spy.call_count == 0
 
 
 def test_2_uses_pandas(mocker):
     sql_spy = mocker.spy(duckdb, "sql")
-    pandas_spy = mocker.spy(pd.DataFrame, "query")
 
     people = pd.DataFrame({"State": ["MO", "MO", "IL"], "Age": [17, 18, 19]})
 
     hw1.avg_per_state(people)
 
     assert sql_spy.call_count == 0
-    assert pandas_spy.call_count == 1
