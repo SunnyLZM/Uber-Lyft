f37d90314827a537ce4f88ab19dcc73c8fd9409c
xiaonanY317 <xiaonan.y@wustl.edu>
2023-09-13 00:59:56 +0000

    1.1.2

--- fl23/hw1.py
+++ fl23/hw1.py
@@ -4,23 +4,19 @@ import pandas as pd
 
 def filter_after_cutoff_age(people: pd.DataFrame, cutoff_age: int) -> pd.DataFrame:
     """Filter a data frame of people to only include people older than a cutoff age."""
-    # Register the dataframe as a temporary table in duckdb
-    conn = duckdb.connect(database=":memory:", read_only=False)
-    conn.register("people", people)
-
+    con = duckdb.connect()
+    con.register("people", people)
     return duckdb.sql(
         f"""
         SELECT
             *
         FROM people
-        WHERE Age >= {cutoff_age}
+        where Age >= {cutoff_age}
         """,
     ).df()
 
-
 def avg_per_state(people: pd.DataFrame) -> pd.DataFrame:
     """Compute the average age per state."""
-    result = people.groupby("State").agg({"Age": "mean"}).reset_index()
-    result = result.rename(columns={"Age": "avg(Age)"})
-
-    return result.reset_index(drop=True)
+    result = people.groupby("State")["Age"].mean().reset_index()
+    result.columns = ["State", "avg(Age)"]
+    return result
