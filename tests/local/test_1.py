f22743b23fdd01f4017c13f8d6ead2a20c469360
Ty Schlichenmeyer <t.schlic@wustl.edu>
2023-09-15 10:41:26 +0000

    rearrange tests

--- tests/local/test_1.py
+++ tests/local/test_1.py
@@ -1,3 +1,4 @@
+import duckdb
 import pandas as pd
 
 from fl23 import hw1
@@ -10,9 +11,9 @@ def test_filter_after_cutoff_age():
     assert all(adults["Age"] >= cutoff_age)
 
 
-def test_avg_per_state():
-    people = pd.DataFrame({"State": ["MO", "MO", "IL"], "Age": [17, 18, 19]})
-    pd.testing.assert_frame_equal(
-        hw1.avg_per_state(people),
-        pd.DataFrame({"State": ["IL", "MO"], "avg(Age)": [19, 17.5]}),
-    )
+def test_1_uses_sql(mocker):
+    sql_spy = mocker.spy(duckdb, "sql")
+
+    hw1.filter_after_cutoff_age(pd.DataFrame({"Age": [17, 18]}), 18)
+
+    assert sql_spy.call_count == 1
--- tests/local/test_2.py
+++ tests/local/test_2.py
@@ -4,12 +4,12 @@ import pandas as pd
 from fl23 import hw1
 
 
-def test_1_uses_sql(mocker):
-    sql_spy = mocker.spy(duckdb, "sql")
-
-    hw1.filter_after_cutoff_age(pd.DataFrame({"Age": [17, 18]}), 18)
-
-    assert sql_spy.call_count == 1
+def test_avg_per_state():
+    people = pd.DataFrame({"State": ["MO", "MO", "IL"], "Age": [17, 18, 19]})
+    pd.testing.assert_frame_equal(
+        hw1.avg_per_state(people),
+        pd.DataFrame({"State": ["IL", "MO"], "avg(Age)": [19, 17.5]}),
+    )
 
 
 def test_2_uses_pandas(mocker):
