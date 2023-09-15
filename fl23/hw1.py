6624ceaea34691bfdab41de0332c361ea2b0dd6d
Ty Schlichenmeyer <t.schlic@wustl.edu>
2023-09-15 12:19:41 +0000

    feat: add submodule for hidden tests

--- fl23/hw1.py
+++ fl23/hw1.py
@@ -18,3 +18,8 @@ def avg_per_state(people: pd.DataFrame) -> pd.DataFrame:  # noqa: ARG001
         GROUP BY State
         """,
     ).df()
+
+
+def generate_people(n: int) -> pd.DataFrame:
+    """Generate a data frame of people with random ages, states, names, and text."""
+    return pd.DataFrame()
