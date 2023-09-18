9fff5a674da0471a51c0d8cf4a987bd37ad28a33
Ty Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2023-09-18 13:58:45 +0000

    adjust function call

--- tests/test_1.py
+++ tests/test_1.py
@@ -16,6 +16,6 @@ def test_filter_after_cutoff_age():
 def test_1_uses_sql(mocker):
     sql_spy = mocker.spy(duckdb, "sql")
 
-    hw1.filter_after_cutoff_age(pd.DataFrame({"Age": [17, 18]}), 18)
+    hw1.filter_after_cutoff_age(pd.DataFrame({"Age": [17, 18]}))
 
     assert sql_spy.call_count == 1
