5c0351b957504136f3e08f493cd39b29a878b2e6
kaohermione <102507412+kaohermione@users.noreply.github.com>
2023-09-19 23:06:31 +0000

    1.3.1

--- tests/test_3.py
+++ tests/test_3.py
@@ -4,7 +4,9 @@ from fl23 import hw1
 
 
 def test_large_states_adult_age():
-    people = pd.DataFrame({"State": ["Missouri","Missouri", "Missouri","Montana"],
+    people = pd.DataFrame({"first_name":["Alice","Bob","Jack","Harry"],
+                           "last_name":["Jane","Pan","Twan","Potter"],
+                           "State": ["Missouri","Missouri", "Missouri","Montana"],
                            "Age": [17, 18, 19, 19]})
     pd.testing.assert_frame_equal(
         hw1.large_states_adult_age(people),
