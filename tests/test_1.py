9518d2d3b892c8809f3690e2000dce9e6506b468
kaohermione <102507412+kaohermione@users.noreply.github.com>
2023-09-19 22:38:09 +0000

    1.1,1.2,1.3,1.4,1.7,1.8

--- tests/test_1.py
+++ tests/test_1.py
@@ -8,7 +8,7 @@ from fl23 import hw1
 def test_filter_after_cutoff_age():
     cutoff_age = 18
     people = pd.DataFrame({"Age": [17, 18]})
-    adults = hw1.filter_after_cutoff_age(people,cutoff_age)
+    adults = hw1.filter_after_cutoff_age(people, cutoff_age)
     assert all(adults["Age"] >= cutoff_age)
 
 
@@ -17,6 +17,6 @@ def test_1_uses_sql(mocker):
     sql_spy = mocker.spy(duckdb, "sql")
     exec_spy = mocker.spy(duckdb, "execute")
 
-    hw1.filter_after_cutoff_age(pd.DataFrame({"Age": [17, 18]}))
+    hw1.filter_after_cutoff_age(pd.DataFrame({"Age": [17, 18]}), 18)
 
     assert (sql_spy.call_count == 1) or (exec_spy.call_count == 1)
