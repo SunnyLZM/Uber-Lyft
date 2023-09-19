560888d2ec6040b656bd2fb22c796eb2c8e28b67
kaohermione <102507412+kaohermione@users.noreply.github.com>
2023-09-19 03:21:40 +0000

    1.7

--- tests/test_7.py
+++ tests/test_7.py
@@ -1,7 +1,7 @@
-# from fl23 import hw1
+from fl23 import hw1
 
 
-# def test_generate_people():
-#     n = 10
-#     people = hw1.generate_people(n)
-#     assert people.columns.tolist() == ["first_name", "last_name", "Age", "State", "random_sentence"]
+def test_generate_people():
+    n = 10
+    people = hw1.generate_people(n)
+    assert people.columns.tolist() == ["first_name", "last_name", "Age", "State", "random_sentence"]
